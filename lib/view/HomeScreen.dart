4615506938680a86bd651d2ccd7d768956523aed
Shahinsha <shahinsha676523k@gmail.com>
2024-08-22 15:09:41 +0530

    initial commit

---
+++ lib/view/HomeScreen.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:image_parallex/model/Data.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  late PageController pageController;
+  double pageOffset = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController(viewportFraction: 0.7);
+    pageController.addListener(() {
+      setState(() {
+        pageOffset = pageController.page!;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/IMG_6204.jpg"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 50),
+                    Text(
+                      'Art Street',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 45,
+                        letterSpacing: 2,
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      '1853',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 20,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'Art Street showcases a vibrant collection of works from emerging and established artists, creating a dynamic hub of creativity and inspiration for all art enthusiasts.',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(left: 20, bottom: 20),
+                    child: Text(
+                      'Highlight Paintings :',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 400,
+                    padding: const EdgeInsets.only(bottom: 30),
+                    child: PageView.builder(
+                      itemCount: paintings.length,
+                      controller: pageController,
+                      itemBuilder: (context, i) {
+                        return Transform.scale(
+                          scale: 1,
+                          child: Container(
+                            padding: const EdgeInsets.only(right: 20),
+                            child: Stack(
+                              children: [
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(15),
+                                  child: Image.asset(
+                                    paintings[i]['image'],
+                                    height: 370,
+                                    fit: BoxFit.cover,
+                                    alignment:
+                                        Alignment(-pageOffset.abs() + i, 0),
+                                  ),
+                                ),
+                                Positioned(
+                                  left: 10,
+                                  bottom: 10,
+                                  right: 10,
+                                  child: Text(
+                                    paintings[i]['name'],
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 35,
+                                      fontStyle: FontStyle.normal,
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
